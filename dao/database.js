16700475e07b2f09fcb706c9488e355aadd1a4e3
Anderson Souza <andersom.eugenio@gmail.com>
2021-05-03 22:26:45 -0300

    adicionado conexao sqlite com sqlite-async (promises)

---
+++ dao/database.js
@@ -0,0 +1,27 @@
+const ARQUIVO_DB = "../adopetme.db";
+var path = require("path");
+var Database = require("sqlite-async");
+var db = new Database();
+var dbOpened = null;
+database = {
+  async open() {
+    if (dbOpened) {
+      return dbOpened;
+    }
+    dbOpened = await db.open(path.join(__dirname, ARQUIVO_DB));
+    return dbOpened;
+  },
+  close() {
+    return db.close();
+  },
+  //   run() {
+  //     let args = arguments;
+  //     return new Promise(function (res, rej) {
+  //       database.open().run(...args, (err) => {
+  //         if (err) rej(err);
+  //         else res();
+  //       });
+  //     });
+  //   },
+};
+module.exports = database;
