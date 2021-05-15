98d88b4be57c930a2e0dfaa884720f5a44cbadc8
Anderson Souza <andersom.eugenio@gmail.com>
2021-05-14 23:07:43 -0300

    logando com usuarios do banco de dados

--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,53 @@
-var express = require('express');
-var http = require('http');
-var jwt = require('jsonwebtoken');
-
-var router = express.Router();
-var SECRET = 'adopetme';
+const express = require("express");
+const http = require("http");
+const jwt = require("jsonwebtoken");
+const UserDAO = require("../dao/UserDAO");
+const router = express.Router();
+const SECRET = "adopetme";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-function verificaJWT(req, res, next){
-  const token = req.headers['x-access-token'];
-  const index = blacklist.findIndex(item => item === token);
-  if(index !== -1) return res.status(401).end();
-
+function verificaJWT(req, res, next) {
+  const token = req.headers["x-access-token"];
+  const index = blacklist.findIndex((item) => item === token);
+  if (index !== -1) return res.status(401).end();
 
   jwt.verify(token, SECRET, (err, decoded) => {
     //erro nos casos de token ausente ou inválido
-    if(err) return res.status(401).end();
+    if (err) return res.status(401).end();
 
     //guardando requisição p/ uso posterior
     req.userId = decoded.userId;
     next();
-  })
+  });
 }
-router.get('/clientes', verificaJWT, (req, res) => {
-  res.json([{id:1, nome:'mathaus'}]);
-})
+router.get("/clientes", verificaJWT, (req, res) => {
+  res.json([{ id: 1, nome: "mathaus" }]);
+});
 
-router.post('/login', (req, res) => {
+router.post("/login", async (req, res) => {
   //Dados viriam do banco de dados, engessei para teste
-  if(req.body.user === 'user@contato.com' && req.body.password === '123'){
-  const token = jwt.sign({userId: 1}, SECRET, {expiresIn: 300});
-  return res.json({auth: true, token});
+  let userDAO = new UserDAO();
+  let users = await userDAO.findByEmail(req.body.email);
+  if (users.length > 0) {
+    let user = users[0];
+    if (await user.isPasswordValid(req.body.password)) {
+      const token = jwt.sign({ userId: 1 }, SECRET, { expiresIn: 300 });
+      return res.json({ auth: true, token });
+    } else {
+      res.status(401).end();
+    }
+  } else {
+    res.status(401).end();
   }
+});
 
-  res.status(401).end();
-})
-
-router.post('/logout', function(req,res) {
-  blacklist.push(req.headers['x-access-token']);
+router.post("/logout", function (req, res) {
+  blacklist.push(req.headers["x-access-token"]);
   res.end;
-})
+});
 
-const server = http.createServer(app);
-server.listen(3000);
 module.exports = router;
