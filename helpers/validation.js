349cbe21251630e3fe1b77eb26f162a05e4ee5d6
Anderson Souza <andersom.eugenio@gmail.com>
2021-05-15 01:12:43 -0300

    validação

--- helpers/validation.js
+++ helpers/validation.js
@@ -8,22 +8,53 @@ function validationMiddleware(req, res, next) {
 }
 
 const registerValidation = [
-  check("email").isEmail(),
-  check("firstName").isLength({ min: 3, max: 255 }),
-  check("lastName").isLength({ min: 3, max: 255 }),
+  check("email").isEmail().withMessage("Não é um email válido"),
+  check("firstName")
+    .isLength({ min: 3, max: 255 })
+    .withMessage("Mínimo 3 caracteres"),
+  check("lastName")
+    .isLength({ min: 3, max: 255 })
+    .withMessage("Mínimo 3 caracteres"),
   check("birthdayDate").isDate().toDate(), // formato padrao YYYY/MM/DD
-  check("password").isLength({ min: 8, max: 60 }).isStrongPassword(),
-  check("document").isLength({ min: 11, max: 11 }),
+  check("password")
+    .isLength({ min: 8, max: 60 })
+    .withMessage("Mínimo de 8 caracteres")
+    .isStrongPassword()
+    .withMessage("Senha fraca"),
+  check("document")
+    .isLength({ min: 11, max: 11 })
+    .withMessage("Documento inválido"),
 
-  check("phone").optional().isMobilePhone(["pt-BR"]),
-  check("facebookProfile").optional().isURL(),
+  check("phone")
+    .optional()
+    .isMobilePhone(["pt-BR"])
+    .withMessage("Telefone inválido"),
+  check("facebookProfile")
+    .optional()
+    .isURL()
+    .withMessage("O perfil do facebook tem que ser uma URL válida"),
 
-  check("address").optional().isLength({ max: 255 }),
-  check("number").optional().isNumeric(),
-  check("complement").optional().isLength({ max: 64 }),
-  check("neighborhood").optional().isLength({ max: 64 }),
-  check("city").optional().isLength({ max: 64 }),
-  check("zipcode").optional().isPostalCode(),
+  check("address")
+    .optional()
+    .isLength({ max: 255 })
+    .withMessage("Máximo de 255 caracteres"),
+  check("number")
+    .optional()
+    .isNumeric()
+    .withMessage("Apenas números permitidos"),
+  check("complement")
+    .optional()
+    .isLength({ max: 64 })
+    .withMessage("Máximo 64 caracteres"),
+  check("neighborhood")
+    .optional()
+    .isLength({ max: 64 })
+    .withMessage("Máximo 64 caracteres"),
+  check("city")
+    .optional()
+    .isLength({ max: 64 })
+    .withMessage("Máximo 64 caracteres"),
+  check("zipcode").optional().isPostalCode(["BR"]).withMessage("CEP inválido"),
   validationMiddleware,
 ];
 
